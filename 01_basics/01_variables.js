75b4898af94e8eb4f36937096ae7edb4b34ff6fe
Shubham Hire <78106975+shubhamhire16@users.noreply.github.com>
2023-12-13 15:25:04 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345; //Cannot change constants
+let accountEmail = "shubhamhire@gmmail.com"; //let can be changed
+var accountPass = "23435"; //var can be changed but is not used anymore due to problems in block scope and functional scope
+accountCity = "Nashik"; //you can also define variables like this but it not recommended
+
+console.table([accountId, accountEmail, accountPass, accountCity]); //table displays data in a tabular format

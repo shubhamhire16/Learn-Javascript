10794ebe43eac1ed4c66db5304ec7fab82ecdda0
Shubham Hire <78106975+shubhamhire16@users.noreply.github.com>
2023-12-15 07:28:19 +0000

    added arrays

---
+++ 02_basics/calculatorProgram.js
@@ -0,0 +1,20 @@
+let num1 = 25
+let num2 = 5
+
+function sum(num1,num2) {
+    return num1+num2;
+}
+function sub(num1,num2) {
+    return num1-num2;
+}
+function mul(num1,num2) {
+    return num1*num2;
+}
+function div(num1,num2) {
+    return num1/num2;
+}
+
+console.log(sum(num1,num2));
+console.log(sub(num1,num2));
+console.log(mul(num1,num2));
+console.log(div(num1,num2));

e0dece766e7212e226ac329ae014aaa80c395959
Shubham Hire <78106975+shubhamhire16@users.noreply.github.com>
2024-01-03 14:40:25 +0000

    Primitive Datatypes Questions Solved

---
+++ 00_cohort/prac1.js
@@ -0,0 +1,4 @@
+//write a program to greet a person given their first and last name
+let firstName = "Shubham"
+let lastName = "Hire"
+console.log(`Hello ${firstName} ${lastName}. Welcome to this Cohort!`);
---
+++ 00_cohort/prac2.js
@@ -0,0 +1,8 @@
+//write a program that greets people on the basis on their gender
+
+let gender = "Male";
+if(gender == "Male"){
+    console.log("Hello Male");
+}else{
+    console.log("Hello Female");
+}
